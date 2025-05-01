baa44dce4c6c453d3e233052344cb6ca9b14588a
Juan Milkah <juanmilkah@gmail.com>
2025-05-01 17:50:59 +0300

    display in a gui

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # png-decoder
 
-## TODO
-
-[ ] Handle PNG scanline filter decoding?
-[ ] Convert the decompressed raw bytes to RGB or RGBA pixels?
-[ ] Save the decoded image or view it with a GUI?
+Decode and Display Png images.
 
 Licensed under the [MIT](LICENSE) License.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-// Png Decoder
-
 use std::{
-    fs,
+    env, fs,
     io::{Cursor, Read},
+    thread,
+    time::Duration,
 };
 
 use flate2::read::ZlibDecoder;
+use minifb::{Window, WindowOptions};
 
+#[allow(dead_code)]
 struct Chunk {
     //  A 4-byte unsigned integer giving the number of bytes in
     //  the chunk's data field.
@@ -21,6 +23,8 @@ struct Chunk {
     crc: [u8; 4],
 }
 
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
 struct Ihdr {
     // give dimensions in pixels
     // zero is invalid dimension
@@ -34,6 +38,7 @@ struct Ihdr {
 }
 
 struct Decoder {
+    header: Option<Ihdr>,
     data: Vec<u8>,
     cursor: usize,
 }
@@ -48,7 +53,11 @@ impl Decoder {
         assert_eq!(sig, [137, 80, 78, 71, 13, 10, 26, 10]);
 
         let data = content[8..].to_vec();
-        Self { data, cursor: 0 }
+        Self {
+            header: None,
+            data,
+            cursor: 0,
+        }
     }
 
     fn next_chunk(&mut self) -> Option<Chunk> {
@@ -98,44 +107,229 @@ impl Decoder {
 
     //  Deflate-compressed datastreams within PNG are stored in the "zlib"
     // format,
-    fn decompress(&self, data: &[u8]) -> Vec<u8> {
+    fn decompress_data(&mut self) {
         let mut decompressed = Vec::new();
-        let mut deflator = ZlibDecoder::new(Cursor::new(data));
+        let mut deflator = ZlibDecoder::new(Cursor::new(&self.data));
         deflator
             .read_to_end(&mut decompressed)
             .expect("Failed to decompress chunk!");
-        decompressed
+        self.data = decompressed;
     }
-}
 
-fn main() {
-    let mut decoder = Decoder::new("image.png");
-    let header = decoder.next_chunk().unwrap();
-    let header = decoder.parse_ihdr(&header.data);
-    println!("Dimensions: {}x{}", header.height, header.width);
+    fn unfilter_scanlines(&mut self) {
+        let header = self.header.clone().unwrap();
+        let bytes_per_pixel: usize = match header.color_type {
+            0 => 1, // grayscale
+            2 => 3, // RGB
+            // 3 => each pixelle is a palette index
+            4 => 2, // Grayscale + alpha
+            6 => 4, // RGB + alpha
+            _ => return,
+        };
 
-    let mut image_data = Vec::new();
+        let stride = header.width as usize * bytes_per_pixel;
+        let mut result = Vec::with_capacity(header.height as usize * stride);
+        let mut prev_scanline = vec![0u8; stride as usize];
 
-    while let Some(chunk) = decoder.next_chunk().as_mut() {
-        let typ = str::from_utf8(&chunk.typ).unwrap();
+        let mut i = 0;
+        for _ in 0..header.height {
+            //  each scanline is preceded by a filter type byte
+            // that specifies the filter algorithm used for that scanline.
+            let filter_type = &self.data[i];
+            i += 1;
 
-        match typ {
-            "IHDR" => {
-                eprintln!("Two Ihdrs");
-                break;
+            let scanline = &self.data[i..i + stride];
+            let mut unfiltered = vec![0u8; stride as usize];
+
+            match filter_type {
+                // None
+                0 => unfiltered.copy_from_slice(scanline),
+                //Sub
+                // Sub(x) + Raw(x-bpp)
+                1 => {
+                    for x in 0..stride {
+                        let left = if x >= bytes_per_pixel {
+                            unfiltered[x - bytes_per_pixel]
+                        } else {
+                            0
+                        };
+
+                        unfiltered[x] = scanline[x].wrapping_add(left);
+                    }
+                }
+                // Up
+                // Up(x) = Raw(x) - Prior(x)
+                2 => {
+                    for x in 0..stride {
+                        unfiltered[x] = scanline[x].wrapping_add(prev_scanline[x]);
+                    }
+                }
+                // Average
+                // Average(x) = Raw(x) - floor((Raw(x-bpp)+Prior(x))/2)
+                3 => {
+                    for x in 0..stride {
+                        let left = if x >= bytes_per_pixel {
+                            unfiltered[x - bytes_per_pixel]
+                        } else {
+                            0
+                        };
+
+                        let up = prev_scanline[x];
+                        let avg = ((left as u16 + up as u16) / 2) as u8;
+
+                        unfiltered[x] = scanline[x].wrapping_add(avg);
+                    }
+                }
+                // Paeth
+                // Paeth(x) = Raw(x) - PaethPredictor(Raw(x-bpp), Prior(x), Prior(x-bpp))
+                4 => {
+                    for x in 0..stride {
+                        let a = if x >= bytes_per_pixel {
+                            unfiltered[x - bytes_per_pixel]
+                        } else {
+                            0
+                        };
+
+                        let b = prev_scanline[x];
+
+                        let c = if x >= bytes_per_pixel {
+                            prev_scanline[x - bytes_per_pixel]
+                        } else {
+                            0
+                        };
+
+                        unfiltered[x] = scanline[x].wrapping_add(self.paeth_predicator(a, b, c));
+                    }
+                }
+
+                _ => panic!("Undupported filter type!"),
             }
-            "IDAT" => {
-                println!("Data chunk with length: {}", chunk.length);
-                image_data.append(&mut chunk.data);
-            }
-            "IEND" => {
-                println!("End Chunk");
-                break;
-            }
-            other => panic!("UKNOWN chunk type: {}", other),
+
+            result.extend_from_slice(&unfiltered);
+            prev_scanline = unfiltered;
+            i += stride;
+        }
+
+        self.data = result;
+    }
+
+    fn paeth_predicator(&self, a: u8, b: u8, c: u8) -> u8 {
+        let a = a as i32;
+        let b = b as i32;
+        let c = c as i32;
+
+        let p = a + b - c;
+        let pa = (p - a).abs();
+        let pb = (p - b).abs();
+        let pc = (p - c).abs();
+
+        if pa <= pb && pa <= pc {
+            return a as u8;
+        } else if pb <= pc {
+            return b as u8;
+        } else {
+            return c as u8;
         }
     }
 
-    let decompressed = decoder.decompress(&image_data);
-    println!("IMAGE DATA: {} BYTES", decompressed.len());
+    fn pixels_to_u32(&self) -> Vec<u32> {
+        let color_type = self.header.clone().unwrap().color_type;
+        let mut buffer = Vec::with_capacity(
+            self.data.len()
+                / match color_type {
+                    2 => 3,
+                    6 => 4,
+                    _ => panic!("Unsupported format!"),
+                },
+        );
+
+        match color_type {
+            2 => {
+                for chunk in self.data.chunks(3) {
+                    let r = chunk[0] as u32;
+                    let g = chunk[1] as u32;
+                    let b = chunk[2] as u32;
+
+                    buffer.push((r << 16) | (g << 8) | b);
+                }
+            }
+            6 => {
+                //   ignore alpha
+                for chunk in self.data.chunks(3) {
+                    let r = chunk[0] as u32;
+                    let g = chunk[1] as u32;
+                    let b = chunk[2] as u32;
+
+                    buffer.push((r << 16) | (g << 8) | b);
+                }
+            }
+            _ => {}
+        }
+        buffer
+    }
+
+    fn display_image(&self, pixels: &[u32]) {
+        let header = self.header.clone().unwrap();
+        let width = header.width as usize;
+        let height = header.height as usize;
+        let mut window = Window::new("PNG Viewer", width, height, WindowOptions::default())
+            .expect("Failed to open window");
+
+        while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+            window
+                .update_with_buffer(pixels, width, height)
+                .expect("Failed to update window");
+        }
+
+        // allow cleanup
+        thread::sleep(Duration::from_millis(100));
+    }
+}
+
+fn help() {
+    println!("USAGE: pigo");
+    println!("  pigo image.png");
+}
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    if args.len() < 1 {
+        help();
+        return;
+    }
+
+    for image in args {
+        let mut decoder = Decoder::new(&image);
+        let header = decoder.next_chunk().unwrap();
+        decoder.header = Some(decoder.parse_ihdr(&header.data));
+
+        let mut image_data = Vec::new();
+
+        while let Some(chunk) = decoder.next_chunk().as_mut() {
+            let typ = str::from_utf8(&chunk.typ).unwrap();
+
+            match typ {
+                "IHDR" => {
+                    eprintln!("Two Ihdrs");
+                    break;
+                }
+                "IDAT" => {
+                    image_data.append(&mut chunk.data);
+                }
+                "IEND" => {
+                    break;
+                }
+                other => panic!("UKNOWN chunk type: {}", other),
+            }
+        }
+
+        decoder.data = image_data;
+        decoder.decompress_data();
+
+        // unfilter the data
+        decoder.unfilter_scanlines();
+        let buffer = decoder.pixels_to_u32();
+        decoder.display_image(&buffer);
+    }
 }
